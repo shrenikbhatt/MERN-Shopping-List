870089158417b4dbd64b456e092f784ead4f7fe0
Shrenik Bhatt <shre1106@gmail.com>
2019-07-30 23:45:19 -0400

    Added authentication functionality

---
+++ client/src/components/auth/Logout.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from "react";
+import { logout } from "../../actions/authActions";
+import { connect } from "react-redux";
+import { NavLink } from "reactstrap";
+import Proptypes from "prop-types";
+
+export class Logout extends Component {
+  static propTypes = {
+    logout: Proptypes.func.isRequired
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <NavLink onClick={this.props.logout} href="#">
+          Logout
+        </NavLink>
+      </Fragment>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { logout }
+)(Logout);
--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://shrenikDB:qwert1234A@mern-shopping-list-c2nc9.gcp.mongodb.net/test?retryWrites=true&w=majority"
-}
